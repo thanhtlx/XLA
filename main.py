7a06320d10b5ac42b5a48896a17e14c43e9c761e
thanhtlx <thanh1908.xx@gmail.com>
2021-12-08 09:48:32 +0700

    build

--- main.py
+++ main.py
@@ -1,8 +1,151 @@
 import os
+import cv2
+from util import *
 
-def table_detection(filename):
-    pass
+
+def table_detection(img_path):
+    img = cv2.imread(img_path)
 
 
-{table_detection('./forms/' + i) for i in os.listdir('./forms/') if i.endswith('.png')
- or i.endswith('.PNG') or i.endswith('.jpg') or i.endswith('.JPG')}
+    # resize if the row space or column space is not good
+    # img = cv2.resize(img, (int(img.shape[1]*2), int(img.shape[0]*2)))
+    img = preprocess(img, 2)
+
+    display_bgr2rgp(img)
+
+    binimg = binarilize(img)
+
+
+    plt.imshow(binimg, cmap='gray')
+    plt.show()
+    borderless = remove_border(binimg, )
+
+
+    plt.imshow(borderless, cmap='gray')
+    plt.show()
+    vertical = horizontal = borderless.copy()
+
+
+    img_height, img_width = horizontal.shape
+    SCALE = 10
+
+
+    horizontal_kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (int(img_width / SCALE), 1))
+    hor_dilate = cv2.dilate(horizontal, horizontal_kernel, iterations=20)
+    _, hor_dilate = cv2.threshold(hor_dilate, 127, 255, cv2.THRESH_BINARY)
+    plt.imshow(hor_dilate, cmap='gray')
+    plt.show()
+    vertical_kernel = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (1, int(img_width / SCALE)))
+
+
+    ver_dilate = cv2.dilate(vertical, vertical_kernel, iterations=20)
+    _, ver_dilate = cv2.threshold(ver_dilate, 127, 255, cv2.THRESH_BINARY)
+    plt.imshow(ver_dilate, cmap='gray')
+    plt.show()
+    bw_and = cv2.bitwise_and(hor_dilate, ver_dilate)
+
+
+    plt.imshow(bw_and, cmap='gray')
+    plt.show()
+    contours, hierarchy = cv2.findContours(
+        bw_and, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+
+    len(contours)
+    bounding_boxes = [cv2.boundingRect(c) for c in contours]
+
+    test = binimg.copy()
+
+
+    for box in bounding_boxes:
+        cv2.rectangle(test, (box[0], box[1]),
+                    (box[0]+box[2], box[1]+box[3]), 255, 2)
+
+    plt.imshow(test, cmap='gray')
+    plt.show()
+    cells = [c for c in bounding_boxes]
+
+
+    rows = []
+    while cells:
+        first = cells[0]
+        rest = cells[1:]
+        cells_in_same_row = sorted(
+            [c for c in rest if cell_in_same_row(c, first)], key=lambda c: c[0])
+
+        row_cells = sorted([first] + cells_in_same_row, key=lambda c: c[0])
+        row_cells = reduce_col(row_cells)
+        rows.append(row_cells)
+        cells = [c for c in rest if not cell_in_same_row(c, first)]
+
+        rows.sort(key=avg_height_of_center)
+        new_rows = []
+    for row in rows:
+        new_row_cell = []
+        for cell in row:
+            cx, cy, cw, ch = cell
+            cell_thresh = borderless[cy:cy+ch, cx:cx+cw]
+
+            ver = hor = cell_thresh.copy()
+            ih, iw = hor.shape
+
+
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            opened = cv2.dilate(cell_thresh, kernel)
+            contours, hierarchy = cv2.findContours(
+                opened, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            bounding_rects = [cv2.boundingRect(c) for c in contours]
+            # hyperparameter tuning here
+            MIN_CHAR_HEIGTH = 4
+            MIN_CHAR_WIDTH = 2
+            char_sized_bounding_rects = [
+                (x, y, w, h) for x, y, w, h in bounding_rects if w >= MIN_CHAR_WIDTH and h >= MIN_CHAR_HEIGTH]
+            FIXED_PX = 1
+            if char_sized_bounding_rects:
+                minx, miny, maxx, maxy = math.inf, math.inf, 0, 0
+                for x, y, w, h in char_sized_bounding_rects:
+                    minx = min(minx, x)
+                    miny = min(miny, y)
+                    maxx = max(maxx, x + w)
+                    maxy = max(maxy, y + h)
+                x, y, w, h = minx, miny, maxx - minx, maxy - miny
+                cropped = (cx+x-FIXED_PX, cy+y-FIXED_PX,
+                        min(iw + FIXED_PX*2, w), min(ih + FIXED_PX*2, h))
+                new_row_cell.append(cropped)
+        if new_row_cell != []:
+            new_rows.append(new_row_cell)
+        test = binimg.copy()
+
+    bnd_rects = ravel(new_rows)
+    for box in bnd_rects:
+        cv.rectangle(test, (box[0], box[1]),
+                    (box[0]+box[2], box[1]+box[3]), 255, 2)
+
+    plt.imshow(test, cmap='gray')
+    plt.show()
+    descartes = []
+
+    # read file 
+    annot_data = pd.read_csv('cell data/19.csv')
+    cell_count = len(annot_data)
+    cell_count
+
+
+    for rect in bnd_rects:
+        for idx in range(cell_count):
+            iou = calculate_iou(rect, annot_data.iloc[idx])
+            descartes.append(iou)
+
+    descartes = np.array(descartes)
+    IOU = np.sort(descartes)[::-1][:cell_count]
+    average = sum(IOU) / cell_count
+    print(average)
+    good_estimated = sum(IOU > 0.5)
+    return good_estimated
+
+
+if __name__ == "__main__":
+    {table_detection('./train/' + i) for i in os.listdir('./train/') if i.endswith('.png')
+    or i.endswith('.PNG') or i.endswith('.jpg') or i.endswith('.JPG')}
--- util.py
+++ util.py
@@ -1,21 +1,19 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image, ImageEnhance
 import math
 import matplotlib.pyplot as plt
 
-
-
 def display_bgr2rgp(img):
-    rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=(10, 10))
     plt.imshow(rgb_img)
     plt.show()
 
 
 def preprocess(img, factor=2):
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = Image.fromarray(img)
     enhancer = ImageEnhance.Sharpness(img).enhance(factor)
     if gray.std() < 30:
@@ -33,3 +31,102 @@ def cell_in_same_row(c1, c2):
 def avg_height_of_center(row):
     centers = [y + h / 2 for x, y, w, h in row]
     return sum(centers) / len(centers)
+
+
+def binarilize(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray, (3, 3), 0, 0)
+    thresh = cv.adaptiveThreshold(
+        ~blur, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5, -2,)
+    # remove border noise
+    cv.rectangle(thresh, (0, 0), thresh.shape[::-1], 0, 3)
+    return thresh
+
+
+def remove_border(thresh, REMOVE_SCALE=3):
+    # get vertical and horizontal lines and remove them
+    vertical = horizontal = thresh.copy()
+    img_height, img_width = horizontal.shape
+
+    horizontal_kernel = cv.getStructuringElement(
+        cv.MORPH_RECT, (int(img_width / REMOVE_SCALE), 1))
+    horizontally_opened = cv.morphologyEx(
+        thresh, cv.MORPH_OPEN, horizontal_kernel)
+    vertical_kernel = cv.getStructuringElement(
+        cv.MORPH_RECT, (1, int(img_height / REMOVE_SCALE)))
+    vertically_opened = cv.morphologyEx(thresh, cv.MORPH_OPEN, vertical_kernel)
+    both = horizontally_opened + vertically_opened
+    both = cv.dilate(both, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)))
+
+    borderless = thresh - both
+    borderless = cv.morphologyEx(
+        borderless, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_CROSS, (2, 2)))
+
+    return borderless
+
+
+def check_combine_column(a, b, MIN_COLUMN_SPACE=12):
+    #x2 > x1
+    x1, y1, w1, h1 = a
+    x2, y2, w2, h2 = b
+
+    col_space = x2 - (x1 + w1) + 1
+    if col_space < MIN_COLUMN_SPACE:
+        new_rect = (x1, min(y1, y2), w2+x2-x1+1,
+                    max(y1+h1, y2+h2) - min(y1, y2) + 1)
+        return new_rect
+    else:
+        return False
+
+def reduce_col(rects, MIN_COLUMN_SPACE=12):
+    cursor = len(rects) - 1
+    while cursor > 0:
+        last = rects[cursor]
+        next_last = rects[cursor-1] if rects[cursor-1] else None
+        if next_last:
+            check = check_combine_column(next_last, last, MIN_COLUMN_SPACE)
+            if check != False:
+                rects.pop(cursor)
+                rects.pop(cursor - 1)
+                rects.insert(cursor-1, check)
+        cursor -= 1
+    return rects
+
+
+def ravel(lst):
+    result = []
+    for row in lst:
+        for cell in row:
+            result.append(cell)
+
+    return result
+
+
+def calculate_iou(predict_bndbox, true_bndbox):
+    """
+    Calculate the IOU between the precdict and the true bounding box
+    Predict (x, y, w, h)
+    """
+
+    x, y, w, h = predict_bndbox
+
+    xmin1, ymin1, xmax1, ymax1 = x, y, x+w, y+h
+    xmin2, xmax2, ymin2, ymax2 = true_bndbox
+
+    xmin = max(xmin1, xmin2)
+    xmax = min(xmax1, xmax2)
+    ymin = max(ymin1, ymin2)
+    ymax = min(ymax1, ymax2)
+
+    inter_width = max(xmax - xmin + 1, 0)
+    inter_height = max(ymax - ymin + 1, 0)
+
+    inter_area = inter_width * inter_height
+
+    pred_area = w * h
+    true_area = (xmax2 - xmin2 + 1) * (ymax2 - ymin2 + 1)
+
+    union_area = pred_area + true_area - inter_area
+
+    IOU = round(inter_area / union_area, 3)
+    return IOU
